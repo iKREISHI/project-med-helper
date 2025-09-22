ee334af66feb49bfe0a424e31bcb6980441a2362
iKREISHI <vadas25@yandex.ru>
2025-09-22 20:25:17 +0500

    #184 отключить csrf

--- backend/src/config/settings/middleware.py
+++ backend/src/config/settings/middleware.py
@@ -14,12 +14,18 @@ CORS_ALLOW_METHODS = [
     "PUT",
 ]
 
-CORS_ALLOWED_ORIGINS = [
+_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost",
+    "http://127.0.0.1",
 ]
+
+CORS_ALLOWED_ORIGINS = _ORIGINS
+
+CSRF_TRUSTED_ORIGINS = _ORIGINS
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
@@ -28,9 +34,10 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'config.custom_middleware.DisableCSRFCheckMiddleware',
 ]
